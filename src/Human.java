d525ea338e096a0284291f31e38640890cc4e50d
ivanb <ivanbears99@gmail.com>
2022-08-31 18:42:59 +0300

    Mess with classes

--- src/Human.java
+++ src/Human.java
@@ -1,3 +1,21 @@
-public class Human {
+public class Human extends Mammal {
+
+    private String job;
+
+
+
+    public Human(String name, int age, String environment, String sex, int height, int weight,String job ) {
+        super(name, age, environment, sex, height, weight);
+
+        this.job = job;
+
+
+    }
+
+
+
+    public String getJob() {        return job;    }
+    public void setJob(String job) {        this.job = job;    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        Animal proteus=new Animal("Proteus", 0, "Earth");
-        System.out.println(proteus.getName());
-        System.out.println(proteus.getAge());
-        System.out.println(proteus.getEnvironment());
+
+
     }
 }
